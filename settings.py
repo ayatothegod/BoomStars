bc149716ad7fae4fbd01c33f3b53dccbbaf0ffeb
ayatothegod <zxcoffee@mail.ru>
2025-03-25 06:33:41 +1000

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,48 @@
+TOKEN = "" #сюда токен бота
+
+admins_id = [
+
+]
+
+SUBGRAM_TOKEN = "" # токен subgram
+HIVIEWS_TOKEN = ""
+
+
+admin_username = "@samopal666" #свой username
+admin_link = "https://t.me/testikboom4ik_bot"
+
+nac_1 = [0.7] # Начисление звезд за 1-ий уровень рефералов
+nac_2 = [1] # Начисление звезд за 2-ий уровень рефералов
+nac_3 = [1.5] # Начисление звезд за 3-ий уровень рефералов
+
+refs_boost = [10]# Количество рефералов для вывода с бустом
+refs_noboost = [15] # Количество рефералов для вывода без буста
+
+task_grant = [0.7] # Награда за задание
+
+click_grant = [0.1] # Награда за клик
+
+boost_cost = [599] # Буст, стоимость
+
+GIFT_AMOUNT = [1.0] # ежедневый гифт
+DAILY_COOLDOWN = 24 * 60 * 60 # не трогать
+DELAY_TIME = 300 # не трогать
+
+#основной канал
+channel_osn = "https://t.me/StarsBoom1"
+#чат
+chater = "https://t.me/StarsBoomChat"
+id_chat = -1002326236324
+#канал выплат
+channel_viplat = "https://t.me/StarsBoomPayout"
+channel_viplat_id = -1002471272629
+#канал выигрышей мини-игры:
+channel_link = "https://t.me/StarsBoomGames"
+id_channel_game = -1002302416665
+
+#Обязательная подписка
+required_subscription = []
+
+#кнопка в старте по запросу
+beta_url = ""
+beta_name = ""
